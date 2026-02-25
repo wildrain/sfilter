5c850e3ecf263a5f299ab0943383b064b8637c11
wildrain <roman.ul.ferdosh@gmail.com>
2026-02-25 17:22:46 +0600

    Update Product constructor to allow AJAX requests for tab and asset initialization

--- includes/Product.php
+++ includes/Product.php
@@ -6,7 +6,7 @@ class Product
 {
     public function __construct()
     {
-        if (!is_admin()) {
+        if (!is_admin() || (defined('DOING_AJAX') && DOING_AJAX)) {
             new Product\Tabs();
             new Product\Assets();
             new Product\Button();
